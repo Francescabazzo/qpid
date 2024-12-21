10755a0ff97661d52b73ab9fffdc5616b56416eb
Edi Codutti <edi.codutti@gmail.com>
2024-12-21 21:27:23 +0100

    1. Solved some indicization bugs 2. Variable deformations happens before clustering 3. Clustering doesn't count age and localization, but does count personal evaluation 4. New matching metric

--- backend/backend.py
+++ backend/backend.py
@@ -2,11 +2,11 @@ import pandas as pd
 import numpy as np
 from sklearn.cluster import HDBSCAN
 from sklearn.neighbors import NearestNeighbors
-from scipy.spatial.distance import cdist, pdist
+from scipy.spatial.distance import cdist
+
+exps = [(1/5), (1/1.3), 1, 2, 5]
 
 def get_matches(df1:pd.DataFrame, search:pd.DataFrame) -> list:
-    exps = [(1/5), (1/1.3), 1, 2, 5]
-
     # ---Preparazione (da modificare)---
     unnecessary = ["name", "bio", "gender", "gender_other", "age_flag_other",
                    "age_radius_other", "distance_flag_other", "distance_km_other",
@@ -47,14 +47,18 @@ def get_matches(df1:pd.DataFrame, search:pd.DataFrame) -> list:
     X = df.to_numpy()
     X = np.vstack((X,search))
 
+    # ---Variable deformation---
+    for cnt, i in enumerate(range(3,8)):
+        X[:-1,i] = np.int64(np.float_power((11-X[:-1,i]), exps[weights[cnt]-1]))
+
     # ---Clustering---
     clustering = HDBSCAN(min_cluster_size=6, n_jobs=-1, store_centers="centroid", allow_single_cluster=True)
-    clustering.fit(X[:, np.r_[5:8, 9:23]])
+    clustering.fit(X[:, np.r_[3:23]])
 
     # re-assign noisy points
     centroids = clustering.centroids_
     noisy = X[clustering.labels_ == -1]
-    noisy = noisy[:,np.r_[5:8, 9:23]]
+    noisy = noisy[:,np.r_[3:23]]
     distmat = cdist(noisy, centroids)
     idxs = np.argmin(distmat, axis=1)
     clustering.labels_[clustering.labels_ == -1] = idxs
@@ -63,19 +67,19 @@ def get_matches(df1:pd.DataFrame, search:pd.DataFrame) -> list:
     X = X[clustering.labels_ == clustering.labels_[-1]]
     IDs = IDs[clustering.labels_ == clustering.labels_[-1]]
 
-    # ---Variable deformation---
-    for cnt, i in enumerate(range(4,9)):
-        X[:-1,i] = np.int64(np.float_power((11-X[:-1,i]), exps[weights[cnt]-1]))
-
     # ---Nearest Neighbors---
     nn = NearestNeighbors(n_jobs=-1)
-    nn.fit(X)
-    idxs = nn.kneighbors(X[-1].reshape(1,-1), return_distance=False, n_neighbors=6)
-
-    # the cosine distance is returned to the user as a percentage
-    # to understand the goodness of the match
-    dsts = pdist(X[idxs[0]], metric="cosine")[0:5]
-    dsts = (2-dsts)/2*100
+    nn.fit(X[:-1])
+    idxs = nn.kneighbors(X[-1].reshape(1,-1), return_distance=False, n_neighbors=5)
     
+    # ---Calculate Scores---
+    # Variable de-deformation
+    for cnt, i in enumerate(range(3,8)):
+        X[:-1,i] = 11-np.float_power((X[:-1,i]), 1/exps[weights[cnt]-1])
+
+    scores = np.absolute(X[idxs[0]]-X[-1])[:,3:]
+    scores[:,5:] = 10-scores[:,5:]
+    scores = np.mean(scores, axis=1)*10
+
     # ---Result---
-    return IDs[idxs[0, 1:]], dsts
+    return IDs[idxs[0]], scores
