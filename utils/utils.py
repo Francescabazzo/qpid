149f04e7f97b07d161b6cb46bc0c55ef3920cef8
Alessio Manià <alessio.mania311@gmail.com>
2025-01-17 22:29:39 +0100

    Chore: code refactoring

--- utils/utils.py
+++ utils/utils.py
@@ -1,10 +1,10 @@
 import math
 
-def calcLatLonRange(lat, lon, distance_km):
-    km_per_degree = 111
+def calc_lat_lon_range(lat, lon, distance_km):
+    KM_PER_DEGREE = 111
 
-    delta_lat = distance_km / km_per_degree
-    delta_lon = distance_km / (km_per_degree * math.cos(math.radians(lat)))
+    delta_lat = distance_km / KM_PER_DEGREE
+    delta_lon = distance_km / (KM_PER_DEGREE * math.cos(math.radians(lat)))
 
     lat_min, lat_max = lat - delta_lat, lat + delta_lat
     lon_min, lon_max = lon - delta_lon, lon + delta_lon
