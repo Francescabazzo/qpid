ae6747954c4f942d16e6aedaaefc46fbdedf8d20
Alessio Manià <alessio.mania311@gmail.com>
2025-01-21 17:42:50 +0100

    Chore: changed log function

--- utils/db/connection.py
+++ utils/db/connection.py
@@ -8,12 +8,12 @@ def connect2db():
     try:
         engine = create_engine(st.secrets['database']['connection_url'])
 
-        log("CONNECTION TO DB", 0, __name__)
+        log("CONNECTION TO DB", 0)
 
         return engine.connect()
     except exc as e:
         st.error(f"An error was encountered while connecting to the database: {e}")
 
-        log(f"CONNECTION TO DB ERROR: {e}", 2, __name__)
+        log(f"CONNECTION TO DB ERROR: {e}", 2)
 
         return None
